f75ccb3070d24405fc2e35e80bf48dcae93c15f3
Esteven Aragón <stevenaragon314@gmail.com>
2024-10-05 20:57:19 -0600

    Update the first files like a practice

---
+++ learning_firsts_steps.py
@@ -0,0 +1,26 @@
+#--------------------------------------------------------#
+# This code has the purpose of teaching the first steps  #
+# to use Git and GitHub. For that reason, we are going   #
+# to create a function. This one will be short,          #
+# because this is a test to practice the meaning of      #
+# git push, fetch, and pull. These are very important    #
+# because they are key to collaborating effectively.     #
+#                                                        #
+#--------------------------------------------------------#
+
+def fibonacci(n: int) -> list:
+    """
+    This function aims to encourage collaboration and practice
+    how to add or modify features in the function. For this 
+    reason, we will work on adding a new feature to this 
+    function, which is related to 'Fibonacci'. It doesn’t 
+    matter if you don’t know how to solve this problem; 
+    this is just practice for Git, so it's okay.
+    
+    ARGS:
+    > n: It’s a whole number 
+    """
+    if (n == 1) or (n == 2):
+        return 1
+    
+    # code ...
